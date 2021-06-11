c20997b19765c3910cc8e63d069aca868d31fdf4
R. Tyler Croy <rtyler@brokenco.de>
2021-06-11 11:50:22 -0700

    Sketching out the design and implementation of Delta S3 Loader
    There is still more to think about but this is a reasonable checkpoint to commit
    and push for others to look at.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 /*
  * The bulk of the application
  */
-
+use clap::{App, Arg};
 use log::*;
-use std::path::PathBuf;
 
-mod config;
-mod writer;
 #[cfg(lambda)]
 mod lambda;
+mod writer;
 
 fn preboot() {
     #[cfg(debug_assertions)]
@@ -18,13 +16,44 @@ fn preboot() {
         "Initializing delta-s3-loader v{}",
         env!["CARGO_PKG_VERSION"]
     );
+    let _matches = App::new("delta-s3-loader")
+        .version(env!["CARGO_PKG_VERSION"])
+        .author("rtyler@brokenco.de")
+        .about("Watch S3 buckets for new data to load into a Delta Table")
+        .arg(
+            Arg::with_name("table")
+                .required(true)
+                .short("t")
+                .long("table")
+                .env("TABLE_PATH")
+                .value_name("TABLE_PATH")
+                .help("Sets the destination Delta Table for the ingested data")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("partitions")
+                .short("p")
+                .long("partitions")
+                .env("CSV_PARTITIONS")
+                .value_name("CSV_PARTITIONS")
+                .help("Ordered list of partition from the source data, e.g. year,month")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("SQS queue ARN")
+                .short("q")
+                .long("queue")
+                .value_name("QUEUE_ARN")
+                .help("ARN of the SQS queue to consume, *required* in standalone mode")
+                .takes_value(true),
+        )
+        .get_matches();
 }
 
 #[cfg(not(lambda))]
 fn main() {
     preboot();
 }
-
 
 #[cfg(lambda)]
 #[tokio::main]
